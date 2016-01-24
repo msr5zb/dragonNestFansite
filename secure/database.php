503e93dfabc7889fa451c4eb510eda6b37907fc9
Michael Rallo <msr5zb@mail.missouri.edu>
2016-01-24 00:16:00 -0600

    host update

--- secure/database.php
+++ secure/database.php
@@ -1,5 +1,5 @@
 <?php
-        DEFINE("HOST","host=localhost");
+        DEFINE("HOST","host=127.0.0.1");
         DEFINE("DBNAME","dbname=dragonnest");
         DEFINE("USERNAME","user=adminxaycqqp");
         DEFINE("PASSWORD","password=3s3vsp2TR2Vr");
