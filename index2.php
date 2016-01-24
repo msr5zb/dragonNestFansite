92e00575e2600a09f6ff94457f4ccafb1446ffb8
Michael Rallo <msr5zb@mail.missouri.edu>
2016-01-24 01:20:14 -0600

    Mysql2

--- index2.php
+++ index2.php
@@ -3,13 +3,27 @@ define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
 
-print('Information: ' . DB_HOST DB_USER DB_PASS DB_PORT . '');
+$dbhost = constant("DB_HOST"); // Host name 
+$dbport = constant("DB_PORT"); // Host port
+$dbusername = constant("DB_USER"); // Mysql username 
+$dbpassword = constant("DB_PASS"); // Mysql password 
+$db_name = constant("DB_NAME"); // Database name 
 
-$mysqlCon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, "", DB_PORT) or die("Error: " . mysqli_error($mysqlCon));
-mysqli_select_db($mysqlCon, DB_NAME) or die("Error: " . mysqli_error($mysqlCon));
-mysql_close();
+print('Information: ' . $dbhost.":".$dbport, $dbusername, $dbpassword . '');
+
+$connection = mysql_connect($dbhost.":".$dbport, $dbusername, $dbpassword);
+
+if(!$connection){
+    echo "Could not connect to database";
+}else{
+    echo "Connect to database!!!!";
+}
+
+//$dbconnection = mysql_select_db($db_name);
+
+
 ?>
 
 <!DOCTYPE html>
