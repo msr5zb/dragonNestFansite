b8636cf6e03299cf57d15c1c80314c74c12c0bbe
Michael Rallo <msr5zb@mail.missouri.edu>
2016-01-24 01:36:15 -0600

    Newest4

--- index4.php
+++ index4.php
@@ -1,6 +1,33 @@
 <?php
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
 
-echo('hello');
+$dbhost = constant("DB_HOST"); // Host name 
+$dbport = constant("DB_PORT"); // Host port
+$dbusername = constant("DB_USER"); // Mysql username 
+$dbpassword = constant("DB_PASS"); // Mysql password 
+$db_name = constant("DB_NAME"); // Database name 
+
+echo("hello");
+echo("host" . $dbhost . "");
+echo("host" . $dbport . "");
+echo("host" . $dbusername . "");
+echo("host" . $dbpassword . "");
+echo("host" . $db_name . "");
+
+
+// we connect to localhost at port 3307
+$link = mysql_connect('127.11.213.130:3306', 'adminYnQRm3V', 'gjvPUuTw9zEs');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
+
+
 
 ?>
 
