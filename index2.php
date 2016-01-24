c8b5585042a50998015fe102b72248b99cc428fe
Michael Rallo <msr5zb@mail.missouri.edu>
2016-01-24 00:54:28 -0600

    Mysql

--- index2.php
+++ index2.php
@@ -1,13 +1,13 @@
 <?php
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
 
-	//Connect To Database
-    $conn_string = "host=localhost port=3306 dbname=dragonnest user=adminxaycqqp password=3s3vsp2TR2Vr";
-	$dbconn = pg_connect($conn_string)or die('Could not connect Sorry: ' . pg_last_error());
-	
-if(!$dbconn) 
-	{
-		echo "<p>Failed to connect to DB</p>\n";
-	}	
+$mysqlCon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, "", DB_PORT) or die("Error: " . mysqli_error($mysqlCon));
+mysqli_select_db($mysqlCon, DB_NAME) or die("Error: " . mysqli_error($mysqlCon));
+mysql_close();
 ?>
 
 <!DOCTYPE html>
