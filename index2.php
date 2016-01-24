d9167f4fc9f5e85be23195ea4177076d27d5f9fe
Michael Rallo <msr5zb@mail.missouri.edu>
2016-01-24 01:04:29 -0600

    Mysql2

--- index2.php
+++ index2.php
@@ -5,6 +5,8 @@ define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
 
+print('Information: ' . DB_HOST DB_USER DB_PASS DB_PORT . '');
+
 $mysqlCon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, "", DB_PORT) or die("Error: " . mysqli_error($mysqlCon));
 mysqli_select_db($mysqlCon, DB_NAME) or die("Error: " . mysqli_error($mysqlCon));
 mysql_close();
